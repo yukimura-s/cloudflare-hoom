db63f38fab50e6c53c81a50be4602231bc93c4fd
yukimura-s <cafereon365@gmail.com>
2025-01-15 23:47:19 +0900

    Add files via upload

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+
+import adapter from '@sveltejs/adapter-cloudflare-workers';
+
+export default {
+	kit: {
+		adapter: adapter({
+	  config: 'wrangler.toml',
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+	})
+	}
+};
